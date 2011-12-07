f782284359576f30c3c86749c7c89e713be7070a
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2011-12-07 23:39:16 +0000

    Correct email header

--- server/MM_SERVER/trunk/MM_SERVER/email.php
+++ server/MM_SERVER/trunk/MM_SERVER/email.php
@@ -1,10 +1,10 @@
 <?php
   $from_mail = 'alert@lost.fr';
   $header   = 'From: "LOST Alert"<'.$from_mail.'>'."\n";
-  $headers .= 'Reply-To: noreply@lost.fr'."\n";
+  $header .= 'Reply-To: noreply@lost.fr'."\n";
   $header  .= 'X-Priority: 1'."\n";
-  $headers .= 'Content-Type: text/plain; charset="iso-8859-1"'."\n";
-  $headers .= 'Content-Transfer-Encoding: 8bit';
+  $header .= 'Content-Type: text/plain; charset="iso-8859-1"'."\n";
+  $header .= 'Content-Transfer-Encoding: 8bit';
   if(isset($_GET['msg']) && !empty($_GET['msg'])) { $msg=$_GET['msg']; } else { $msg="UNKNOWN ALERT"; }
   $sujet = 'LOST Alert : '.$msg;
   $message = $sujet."\n".'Sent by '.$from_mail.' at '.date('h:i:s d.m.Y');
