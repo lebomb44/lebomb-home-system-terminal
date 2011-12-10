7c03605963ac59073f0580377713513bbf71b5e5
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2011-12-10 12:40:01 +0000

    Remove new line at end of header because the server add others headers

--- server/MM_SERVER/trunk/MM_SERVER/email.php
+++ server/MM_SERVER/trunk/MM_SERVER/email.php
@@ -9,7 +9,6 @@
   $header .= 'X-Priority: 1'."\r\n";
   $header .= 'X-MSMail-Priority: High'."\r\n";
   $header .= 'Importance: High'."\r\n";
-  $header .= "\r\n";
   if(isset($_GET['msg']) && !empty($_GET['msg'])) { $msg=$_GET['msg']; } else { $msg="UNKNOWN ALERT"; }
   $sujet = 'LOST Alert : '.$msg;
   $message = $sujet."\r\n".'Sent by '.$from_mail.' at '.date('h:i:s  d.m.Y')."\r\n-- LOST\r\n";
