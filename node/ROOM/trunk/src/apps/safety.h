dd7c864a1c5d660d70d26f9be1d62328ac0d1b2a
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2011-12-19 22:49:50 +0000

    Add ROOM node

--- node/ROOM/trunk/src/apps/safety.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SAFETY_
-#define _SAFETY_
-
-extern void safety_init(void);
-extern void safety_cycle(void);
-
-#endif
--- node/ROOM/trunk/src/devices/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIMER_
-#define _TIMER_
-
-extern void timer_init(void);
-extern void timer_start(void);
-extern void timer_stop(void);
-extern void sleep_mode(void);
-
-#endif
