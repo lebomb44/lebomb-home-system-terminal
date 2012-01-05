8d5db3daa8404ed663b5e6280c24343327269ee6
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-01-05 09:52:45 +0000

    Error on prototype

--- node/ROOM/trunk/src/devices/uart.h
+++ node/ROOM/trunk/src/devices/uart.h
@@ -5,7 +5,7 @@
 
 extern void uart_init(void);
 extern void uart_putc(u08 data);
-extern void uart_send(u08 data, u08 nb);
+extern void uart_send(u08 *data, u08 nb);
 extern void uart_puts(u08 *s);
 extern void uart_printf(const unsigned char *, ...);
 
