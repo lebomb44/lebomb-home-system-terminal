39769bd2e46748b7b72a5be4cf498031f8e97ff4
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-01-05 16:41:45 +0000

    Create dedicated file for the RF interface

--- server/MM_SERVER/trunk/MM_SERVER/services/monitor.h
+++ server/MM_SERVER/trunk/MM_SERVER/services/monitor.h
@@ -1,14 +1,12 @@
 #ifndef _MONITOR_H_
 #define _MONITOR_H_
 
-#include <sys/thread.h>
 #include <stdio.h>
 #include <pro/httpd.h>
 
 __BEGIN_DECLS
 
 extern uint8_t mon_init(void);
-extern void Mond(void *arg) __attribute__ ((noreturn));
 extern int mon_show(FILE * stream, REQUEST * req);
 
 __END_DECLS
