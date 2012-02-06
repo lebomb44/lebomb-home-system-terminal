e9ad067ed500cd25a64705ae54a07dfccf1db4d4
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-02-06 22:23:13 +0000

    Unlimited transmission

--- node/REMOTE/trunk/src/apps/remote.c
+++ node/REMOTE/trunk/src/apps/remote.c
@@ -6,20 +6,17 @@
 void remote_init(void)
 {
   /* Buttons */
-  cbi(DDRD,4); sbi(PORTD,4);
+  cbi(DDRC,0); sbi(PORTC,0);
   cbi(DDRD,6); sbi(PORTD,6);
-  cbi(DDRD,3); sbi(PORTD,3);
+  cbi(DDRC,1); sbi(PORTC,1);
   cbi(DDRD,5); sbi(PORTD,5);
-  cbi(DDRD,2); sbi(PORTD,2);
+  cbi(DDRC,2); sbi(PORTC,2);
   cbi(DDRB,7); sbi(PORTB,7);
-  cbi(DDRD,1); sbi(PORTD,1);
+  cbi(DDRC,4); sbi(PORTC,4);
   cbi(DDRB,6); sbi(PORTB,6);
-
-  /* LED */
-  sbi(DDRB,0); sbi(PORTB,0);
 }
 
-#define REMOTE_HEADER_SIZE       (1 + NODE_REG_RF_TX_CTRL - NODE_REG_RF_TX_SRC_ADDR)
+#define REMOTE_HEADER_SIZE       (4)
 #define REMOTE_REG_LB_TX_DATA_NB (1 + REMOTE_HEADER_SIZE - 1)
 #define REMOTE_REG_LB_TX_CKSUM   (REMOTE_REG_LB_TX_DATA_NB + buff[REMOTE_REG_LB_TX_DATA_NB] + 1)
 
@@ -27,11 +24,28 @@ void remote_init(void)
 
 void remote_cycle(void)
 {
+  u08 scene = 0;
+  u08 state = 0;
   u08 i = 0;
   u08 buff[1 + REMOTE_HEADER_SIZE + NODE_RF_DATA_NB + 1] = { 0 };
 
-  /* Set LED ON */
-  sbi(PORTB,0);
+  /* Get the pressed button */
+  /* Button 1 ON */
+  if(!bit_is_set(PINC,0)) { /* SCENE 0 */ scene = 0;   /* STATE ON */  state = 1; }
+  /* Button 1 OFF */
+  if(!bit_is_set(PIND,6)) { /* SCENE 0 */ scene = 0;   /* STATE OFF */  state = 0; }
+  /* Button 2 ON */
+  if(!bit_is_set(PINC,1)) { /* SCENE 1 */ scene = 1;   /* STATE ON */  state = 1; }
+  /* Button 2 OFF */
+  if(!bit_is_set(PIND,5)) { /* SCENE 1 */ scene = 1;   /* STATE OFF */  state = 0; }
+  /* Button 3 ON */
+  if(!bit_is_set(PINC,2)) { /* SCENE 2 */ scene = 2;   /* STATE ON */  state = 1; }
+  /* Button 3 OFF */
+  if(!bit_is_set(PINB,7)) { /* SCENE 2 */ scene = 2;   /* STATE OFF */  state = 0; }
+  /* Button 4 ON */
+  if(!bit_is_set(PINC,4)) { /* SCENE 3 */ scene = 3;   /* STATE ON */  state = 1; }
+  /* Button 4 OFF */
+  if(!bit_is_set(PINB,6)) { /* SCENE 3 */ scene = 3;   /* STATE OFF */  state = 0; }
 
   /* Set synchro word */
   buff[0] = 0xAA;
@@ -43,30 +57,14 @@ void remote_cycle(void)
   buff[3] = REMOTE_CMD_SCENE;
   /* Set number of data words */
   buff[REMOTE_REG_LB_TX_DATA_NB] = 2;
-  /* Get the pressed button */
-  /* Button 1 ON */
-  if(!bit_is_set(PIND,4)) { /* SCENE 0 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 0;   /* SCENE ON */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 1; }
-  /* Button 1 OFF */
-  if(!bit_is_set(PIND,6)) { /* SCENE 0 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 0;   /* SCENE OFF */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 0; }
-  /* Button 2 ON */
-  if(!bit_is_set(PIND,3)) { /* SCENE 1 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 1;   /* SCENE ON */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 1; }
-  /* Button 2 OFF */
-  if(!bit_is_set(PIND,5)) { /* SCENE 1 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 1;   /* SCENE OFF */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 0; }
-  /* Button 3 ON */
-  if(!bit_is_set(PIND,2)) { /* SCENE 2 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 2;   /* SCENE ON */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 1; }
-  /* Button 3 OFF */
-  if(!bit_is_set(PINB,7)) { /* SCENE 2 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 2;   /* SCENE OFF */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 0; }
-  /* Button 4 ON */
-  if(!bit_is_set(PIND,1)) { /* SCENE 3 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 3;   /* SCENE ON */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 1; }
-  /* Button 4 OFF */
-  if(!bit_is_set(PINB,6)) { /* SCENE 3 */ buff[REMOTE_REG_LB_TX_DATA_NB+1] = 3;   /* SCENE OFF */  buff[REMOTE_REG_LB_TX_DATA_NB+2] = 0; }
+  /* Set the SCENE number */
+  buff[REMOTE_REG_LB_TX_DATA_NB+1] = scene;
+  /* Set the state */
+  buff[REMOTE_REG_LB_TX_DATA_NB+2] = state;
   /* Erase checksum */
   buff[REMOTE_REG_LB_TX_CKSUM] = 0;
   /* Compute checksum */
   for(i=0; i<(REMOTE_HEADER_SIZE + buff[REMOTE_REG_LB_TX_DATA_NB]); i++) { buff[REMOTE_REG_LB_TX_CKSUM] = buff[REMOTE_REG_LB_TX_CKSUM] + buff[1+i]; }
   /* Send the frame 10 times */
-  for(i=0; i<10; i++) { uart_send(buff, 1 + REMOTE_HEADER_SIZE + buff[REMOTE_REG_LB_TX_DATA_NB] + 1); }
-
-  /* Set LED OFF */
-  cbi(PORTB,0);
+  while(1) { uart_send(buff, 1 + REMOTE_HEADER_SIZE + buff[REMOTE_REG_LB_TX_DATA_NB] + 1); }
 }
