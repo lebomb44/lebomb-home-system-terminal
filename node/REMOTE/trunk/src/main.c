d21449f03b7d10fde4a367b256ffb140ba2c84ee
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-02-06 22:24:12 +0000

    Remove unecessary code

--- node/REMOTE/trunk/src/main.c
+++ node/REMOTE/trunk/src/main.c
@@ -1,29 +1,19 @@
 #include "global.h"
 
 #include "devices/eeprom.h"
-#include "devices/ir.h"
-#include "devices/timer.h"
 #include "devices/uart.h"
 
 #include "apps/remote.h"
 
 int main(void)
 {
-  uart_init();
-  ir_init();
-  eeprom_init();
-  timer_init();
-
   remote_init();
-
-  sleep_mode();
-  sbi(SREG,7);
-  //timer_start();
+  uart_init();
+  eeprom_init();
 
   remote_cycle();
-  while(1)
-  {
-    sleep();
-  }
+
+  while(1){}
+
   return 0;
 }
