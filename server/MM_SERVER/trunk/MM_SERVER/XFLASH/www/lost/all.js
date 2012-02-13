f673c0cf9b1a776ee66be85d662da528d77f22b1
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-02-13 20:01:06 +0000

    Add power management in the full event list

--- server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/all.js
+++ server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/all.js
@@ -552,7 +552,11 @@ var EVENT_CHAUFFAGE_SDB  = 2;
 var EVENT_CHAUFFAGE_CAFE = 3;
 var EVENT_LUMIERE_AUTO   = 4;
 var EVENT_MUSIQUE_AUTO   = 5;
-var EVENT_MAX            = 6;
+var EVENT_POWER_0        = 6;
+var EVENT_POWER_1        = 7;
+var EVENT_POWER_2        = 8;
+var EVENT_POWER_3        = 9;
+var EVENT_MAX            = 10;
 
 var EVENT_STATUS_OFF = 0;
 var EVENT_STATUS_ON  = 1;
@@ -598,6 +602,10 @@ function lost_ev_xml_get(xml)
   lost_ev_xml_update(xml, EVENT_CHAUFFAGE_CAFE);
   lost_ev_xml_update(xml, EVENT_LUMIERE_AUTO);
   lost_ev_xml_update(xml, EVENT_MUSIQUE_AUTO);
+  lost_ev_xml_update(xml, EVENT_POWER_0);
+  lost_ev_xml_update(xml, EVENT_POWER_1);
+  lost_ev_xml_update(xml, EVENT_POWER_2);
+  lost_ev_xml_update(xml, EVENT_POWER_3);
 }
 
 /* ************* SET ************************ */
@@ -689,6 +697,7 @@ document.write("\
 document.write("\
 <div class=\"iLayer\" id=\"waScenes\" title=\"Scenes\">\
     <div class=\"iMenu\">\
+        <legend></legend>\
         <ul class=\"iArrow\">\
             <li class=\"iRadio\"><img class=\"picto\" src=\""+lost_icons_path+"ambiance.jpg\"/>Ambiances\
                 <label><input type=\"radio\" name=\"radioAmbiances\" onClick=\"lost_atm_set(ATM_CINEMA);\">Cinema</label>\
@@ -697,12 +706,24 @@ document.write("\
             </li>\
             <li><a href=\"#_Evenements\"><img class=\"picto\" src=\""+lost_icons_path+"wakeup.jpg\"/>Evenements</a></li>\
         </ul>\
-        <ul>\
-            <li><a href=\"javascript:lost_scene_set(EVENT_REVEIL, EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_REVEIL, EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"sunrise.jpg\">Reveil</li>\
-            <li><a href=\"javascript:lost_scene_set(EVENT_COUCHE, EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_COUCHE, EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"lit.jpg\">Couche</li>\
-            <li><a href=\"javascript:lost_scene_set(EVENT_MAX+0 , 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_MAX+0 , 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"net.jpg\">WIFI</li>\
-            <li><a href=\"javascript:lost_scene_set(EVENT_MAX+1 , 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_MAX+1 , 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"net.jpg\">Ethernet</li>\
-            <li><a href=\"javascript:lost_scene_set(EVENT_MAX+2 , 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_MAX+2 , 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"elec.jpg\">POWER2</li>\
+        <legend>Rooms</legend>\
+        <ul class=\"iArrow\">\
+            <li><a href=\"javascript:lost_scene_set(EVENT_REVEIL        , EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_REVEIL        , EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"sunrise.jpg\">Reveil</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_COUCHE        , EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_COUCHE        , EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"lit.jpg\">Couche</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_CHAUFFAGE_SDB , EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_CHAUFFAGE_SDB , EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"temperature.jpg\">Chauffage SdB</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_CHAUFFAGE_CAFE, EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_CHAUFFAGE_CAFE, EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"coffee.jpg\">Chauffage Cafe</li>\
+        </ul>\
+        <legend>Modes</legend>\
+        <ul class=\"iArrow\">\
+            <li><a href=\"javascript:lost_scene_set(EVENT_LUMIERE_AUTO  , EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_LUMIERE_AUTO  , EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"lamp.jpg\">Lumiere automatique</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_MUSIQUE_AUTO  , EVENT_STATUS_OFF);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_MUSIQUE_AUTO  , EVENT_STATUS_ON);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"hifi.jpg\">Musique automatique</li>\
+        </ul>\
+        <legend>Power</legend>\
+        <ul class=\"iArrow\">\
+            <li><a href=\"javascript:lost_scene_set(EVENT_POWER_0, 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_POWER_0, 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"net.jpg\">Wifi</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_POWER_1, 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_POWER_1, 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"net.jpg\">Ethernet</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_POWER_2, 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_POWER_2, 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"elec.jpg\">POWER 2</li>\
+            <li><a href=\"javascript:lost_scene_set(EVENT_POWER_3, 0);\" class=\"iButton iBAction\" style=\"width:60px\">OFF</a><a href=\"javascript:lost_scene_set(EVENT_POWER_3, 1);\" class=\"iButton iBWarn\" style=\"width:60px\">ON</a><img class=\"picto\" src=\""+lost_icons_path+"elec.jpg\">POWER 3</li>\
         </ul>\
     </div>\
 </div>\
@@ -1140,6 +1161,15 @@ document.write("\
                 <li><a href=\"#_Event"+EVENT_MUSIQUE_AUTO+"\"><img class=\"picto\" src=\""+lost_icons_path+"hifi.jpg\">Musique automatique</a></li>\
             </ul>\
         </fieldset>\
+        <fieldset>\
+            <legend>Power</legend>\
+            <ul class=\"iArrow\">\
+                <li><a href=\"#_Event"+EVENT_POWER_0+"\"><img class=\"picto\" src=\""+lost_icons_path+"net.jpg\">Wifi</a></li>\
+                <li><a href=\"#_Event"+EVENT_POWER_1+"\"><img class=\"picto\" src=\""+lost_icons_path+"net.jpg\">Ethernet</a></li>\
+                <li><a href=\"#_Event"+EVENT_POWER_2+"\"><img class=\"picto\" src=\""+lost_icons_path+"elec.jpg\">Power 2</a></li>\
+                <li><a href=\"#_Event"+EVENT_POWER_3+"\"><img class=\"picto\" src=\""+lost_icons_path+"elec.jpg\">Power 3</a></li>\
+            </ul>\
+        </fieldset>\
     </div>\
 </div>\
 ");
@@ -1192,3 +1222,7 @@ printEventInfo(EVENT_CHAUFFAGE_SDB , "Chauffage Salle de Bain");
 printEventInfo(EVENT_CHAUFFAGE_CAFE, "Chauffage Cafe"         );
 printEventInfo(EVENT_LUMIERE_AUTO  , "Lumiere automatique"    );
 printEventInfo(EVENT_MUSIQUE_AUTO  , "Musique automatique"    );
+printEventInfo(EVENT_POWER_0       , "Wifi"                   );
+printEventInfo(EVENT_POWER_1       , "Ethernet"               );
+printEventInfo(EVENT_POWER_2       , "Power 2"                );
+printEventInfo(EVENT_POWER_3       , "Power 3"                );
