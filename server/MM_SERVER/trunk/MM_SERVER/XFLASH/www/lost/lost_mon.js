2e348386455ea1b2b52caf2889c2d0f63c1dbb59
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-02-15 10:56:03 +0000

    Add system time update

---
+++ server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/lost_mon.js
@@ -0,0 +1,40 @@
+function lost_mon_xml_get(xml)
+{
+  var out_id = document.getElementById("waHeadTitle");
+  if(out_id)
+  {
+	var in_id = xml.getElementsByTagName("Mon")[0];
+    if(in_id1)
+    {
+      var wday = in_id.getElementsByTagName("wday")[0];
+      var mon = in_id.getElementsByTagName("mon")[0];
+      var mday = in_id.getElementsByTagName("mday")[0];
+      var hour = in_id.getElementsByTagName("hour")[0];
+      var min = in_id.getElementsByTagName("min")[0];
+      var sec = in_id.getElementsByTagName("sec")[0];
+      var year = in_id.getElementsByTagName("year")[0];
+      if(wday && mon && mday && hour && min && sec && year)
+      {
+        out_id.innerHTML = wday.firstChild.nodeValue + " " \
+                         + mon.firstChild.nodeValue + " " \
+                         + mday.firstChild.nodeValue + " " \
+                         + hour.firstChild.nodeValue + ":" \
+                         + min.firstChild.nodeValue + ":" \
+                         + sec.firstChild.nodeValue + " " \
+                         + year.firstChild.nodeValue;
+        %d %02d %02d %02d:%02d:%02d %04d
+        lost_wa_refresh("waHeadTitle");
+      }
+      else
+      {
+        out_id.innerHTML="Unknown";
+        lost_wa_refresh("waHeadTitle");
+      }
+    }
+    else
+    {
+      out_id.innerHTML="Unknown";
+      lost_wa_refresh("waHeadTitle");
+    }
+  }
+}
