aa46a307a072cb9748c1d0b8f152c4db0116f2ae
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-02-15 10:56:20 +0000

    Add system time update

--- server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/all.js
+++ server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/all.js
@@ -253,6 +253,7 @@ function indexLoading(evt)
         xml=xhr.responseXML;
         if(xml)
         {
+          lost_mon_xml_get(xml);
           lost_alarm_xml_get(xml);
           lost_safety_xml_get(xml);
           lost_rooms_xml_get(xml);
@@ -273,6 +274,46 @@ function indexLoading(evt)
 }
 WA.AddEventListener("load", indexLoading);
 //document.onReady=indexLoading();
+function lost_mon_xml_get(xml)
+{
+  var out_id = document.getElementById("waHeadTitle");
+  if(out_id)
+  {
+	var in_id = xml.getElementsByTagName("Mon")[0];
+    if(in_id1)
+    {
+      var wday = in_id.getElementsByTagName("wday")[0];
+      var mon = in_id.getElementsByTagName("mon")[0];
+      var mday = in_id.getElementsByTagName("mday")[0];
+      var hour = in_id.getElementsByTagName("hour")[0];
+      var min = in_id.getElementsByTagName("min")[0];
+      var sec = in_id.getElementsByTagName("sec")[0];
+      var year = in_id.getElementsByTagName("year")[0];
+      if(wday && mon && mday && hour && min && sec && year)
+      {
+        out_id.innerHTML = wday.firstChild.nodeValue + " " \
+                         + mon.firstChild.nodeValue + " " \
+                         + mday.firstChild.nodeValue + " " \
+                         + hour.firstChild.nodeValue + ":" \
+                         + min.firstChild.nodeValue + ":" \
+                         + sec.firstChild.nodeValue + " " \
+                         + year.firstChild.nodeValue;
+        %d %02d %02d %02d:%02d:%02d %04d
+        lost_wa_refresh("waHeadTitle");
+      }
+      else
+      {
+        out_id.innerHTML="Unknown";
+        lost_wa_refresh("waHeadTitle");
+      }
+    }
+    else
+    {
+      out_id.innerHTML="Unknown";
+      lost_wa_refresh("waHeadTitle");
+    }
+  }
+}
 var url_alarm="cgi/alarm.cgi?";
 
 function lost_alarm_perimeter_set()
