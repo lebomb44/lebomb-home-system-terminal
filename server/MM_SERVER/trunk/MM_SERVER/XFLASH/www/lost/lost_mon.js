e493cb4e200aee09b8853e78b0da6634e3a0cd4c
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-02-15 19:51:41 +0000

    Better format for user time

--- server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/lost_mon.js
+++ server/MM_SERVER/trunk/MM_SERVER/XFLASH/www/lost/lost_mon.js
@@ -1,10 +1,11 @@
 function lost_mon_xml_get(xml)
 {
-  var out_id = document.getElementById("waHeadTitle");
-  if(out_id)
+  var out_id1 = document.getElementById("lost_title");
+  var out_id2 = document.getElementById("lost_title");
+  if(out_id1 && out_id2)
   {
 	var in_id = xml.getElementsByTagName("Mon")[0];
-    if(in_id1)
+    if(in_id)
     {
       var wday = in_id.getElementsByTagName("wday")[0];
       var mon = in_id.getElementsByTagName("mon")[0];
@@ -15,24 +16,30 @@ function lost_mon_xml_get(xml)
       var year = in_id.getElementsByTagName("year")[0];
       if(wday && mon && mday && hour && min && sec && year)
       {
-        out_id.innerHTML = wday.firstChild.nodeValue + " " \
-                         + mon.firstChild.nodeValue  + " " \
-                         + mday.firstChild.nodeValue + " " \
-                         + hour.firstChild.nodeValue + ":" \
-                         + min.firstChild.nodeValue  + ":" \
-                         + sec.firstChild.nodeValue  + " " \
-                         + year.firstChild.nodeValue;
+        out_id1.innerHTML = "LOST "
+                         + hour.firstChild.nodeValue + ":"
+                         + min.firstChild.nodeValue  + ":"
+                         + sec.firstChild.nodeValue  + " "
+                         + year.firstChild.nodeValue + "-"
+                         + mon.firstChild.nodeValue  + "-"
+                         + mday.firstChild.nodeValue;
+        out_id2.innerHTML = out_id1.innerHTML;
+        lost_wa_refresh("lost_title");
         lost_wa_refresh("waHeadTitle");
       }
       else
       {
-        out_id.innerHTML="Unknown";
+        out_id1.innerHTML="Unknown";
+        out_id2.innerHTML = out_id1.innerHTML;
+        lost_wa_refresh("lost_title");
         lost_wa_refresh("waHeadTitle");
       }
     }
     else
     {
-      out_id.innerHTML="Unknown";
+      out_id1.innerHTML="Unknown";
+      out_id2.innerHTML = out_id1.innerHTML;
+      lost_wa_refresh("lost_title");
       lost_wa_refresh("waHeadTitle");
     }
   }
