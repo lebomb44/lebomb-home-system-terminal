40ab040a9d61b2f93e6b848331a653487adcc803
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-03-19 22:10:04 +0000

    Users configuration file

---
+++ server/MM_SERVER/trunk/MM_SERVER/config.h
@@ -0,0 +1,10 @@
+#ifdef MMS
+#define LOST_GSM1 "0689350159"
+#define LOST_GSM2 "0689350159"
+#define LOST_EMAIL "email_mms.php?msg="
+#endif
+#ifdef BT
+#define LOST_GSM1 "0689350159"
+#define LOST_GSM2 "0689350159"
+#define LOST_EMAIL "email_bt.php?msg="
+#endif
