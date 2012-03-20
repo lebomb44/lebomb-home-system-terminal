6a7b923bcfd6199d33723998d19b07a01cc9c5fa
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-03-20 14:59:18 +0000

    File dedicated to specific configuration

--- server/MM_SERVER/trunk/MM_SERVER/config.h
+++ server/MM_SERVER/trunk/MM_SERVER/config.h
@@ -1,9 +1,9 @@
-#ifdef MMS
+#ifdef LOST_CONFIG_mms
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
 #define LOST_EMAIL "email_mms.php?msg="
 #endif
-#ifdef BT
+#ifdef LOST_CONFIG_bt
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
 #define LOST_EMAIL "email_bt.php?msg="
