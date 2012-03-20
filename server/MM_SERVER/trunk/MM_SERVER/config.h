c4751a8d0a65736dd736ec0b81eee6dc95f59091
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-03-20 15:05:28 +0000

    Protect header file against multiple includes

--- server/MM_SERVER/trunk/MM_SERVER/config.h
+++ server/MM_SERVER/trunk/MM_SERVER/config.h
@@ -1,3 +1,6 @@
+#ifndef _LOST_CONFIG_H_
+#define _LOST_CONFIG_H_
+
 #ifdef LOST_CONFIG_mms
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
@@ -7,4 +10,6 @@
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
 #define LOST_EMAIL "email_bt.php?msg="
+#endif
+
 #endif
