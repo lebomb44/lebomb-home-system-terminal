243cdec195c47f16d5c45d2e52bea0925b130d00
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-03-29 17:20:33 +0000

    Specific scripts for M&M

---
+++ node/ROOM/trunk/src/config.h
@@ -0,0 +1,10 @@
+#ifndef _NODE_ROOM_CONFIG_H_
+#define _NODE_ROOM_CONFIG_H_
+
+#ifdef NODE_ROOM_CONFIG_mms_c1
+#define LOST_GSM1 "0689350159"
+#define LOST_GSM2 "0689350159"
+#define LOST_EMAIL "email_mms.php?msg="
+#endif
+
+#endif
