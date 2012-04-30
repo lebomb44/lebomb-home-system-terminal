98bc439540d1d3019bffe357a93016def020f26e
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-04-30 15:27:46 +0000

    PUSH URL added

--- server/LOST/trunk/LOST/config.h
+++ server/LOST/trunk/LOST/config.h
@@ -5,11 +5,13 @@
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
 #define LOST_EMAIL "email_mms.php?msg="
+#define LOST_INSERT "insert_mms.php"
 #endif
 #ifdef LOST_CONFIG_bt
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
 #define LOST_EMAIL "email_bt.php?msg="
+#define LOST_INSERT "insert_bt.php"
 #endif
 
 #endif
