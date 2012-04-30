161e1ef8ecc65b3489eb44433fae4942c001107e
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-04-30 14:30:08 +0000

    Use stream instead of raw socket

--- server/LOST/trunk/LOST/apps/media/freebox.c
+++ server/LOST/trunk/LOST/apps/media/freebox.c
@@ -19,60 +19,45 @@ uint8_t freebox_init(void)
 
 int freebox_form(FILE * stream, REQUEST * req)
 {
-  char* buff_send = NULL;
-  char* buff_recv = NULL;
+  TCPSOCKET *sock_out = NULL;
+  FILE *stream_out = NULL;
   int nb = 0;
   int i;
   char *name=NULL;
   char *value=NULL;
-  size_t len = 0;
 
   NutHttpSendHeaderTop(stream, req, 200, "Ok");
   NutHttpSendHeaderBottom(stream, req, "text/html", -1);
 
-  /* Alloc the buffer requered for the request to send */
-  buff_send = malloc(FREEBOX_REQUEST_SIZE_MAX);
-  /* If the allocation failed return in error */
-  if(buff_send == NULL)
+  if(http_request_get_start("212.027.040.254", 80, &sock_out, &stream_out) == 0)
   {
-    return -1;
+    /* Create the HTTP request beginning */
+    fputs("pub/remote_control?", stream_out);
+    /* Count the arguments in the request */
+    nb = NutHttpGetParameterCount(req);
+    /* Build the new request with all the arguments coming from the request */
+    for (i=0; i<nb; i++)
+    {
+      /* Get the a parameter name */
+      name = NutHttpGetParameterName(req, i);
+      /* Get the parameter value */
+      value = NutHttpGetParameterValue(req, i);
+      /* Check the received parameters */
+      if((name != NULL) && (value != NULL))
+      {
+        /* Add a separator only if it is not the first parameter */
+        if(i>0) { fputs("&", stream_out); }
+        /* Concat this name */
+        fputs(name, stream_out);
+        /* Concat the separator */
+        fputs("=", stream_out);
+        /* Concat this value */
+        fputs(value, stream_out);
+      }
+    }
+    http_request_get_end(NULL, stream_out);
+    http_request_close(&sock_out, &stream_out);
   }
 
-  /* Create the HTTP request begining */
-  strncpy(buff_send, "pub/remote_control?", 19+1);
-  /* Count the arguments in the request */
-  nb = NutHttpGetParameterCount(req);
-  /* Build the new request with all the arguments comming from the request */
-  for (i=0; i<nb; i++)
-  {
-    /* Get the a parameter name */
-    name = NutHttpGetParameterName(req, i);
-    /* Get the parameter value */
-    value = NutHttpGetParameterValue(req, i);
-    /* Get the space left */
-    len = strnlen(buff_send, FREEBOX_REQUEST_SIZE_MAX);
-    /* Only concat if the parmeter separator (if required) name, separator, value and http end are not too big */
-    if(((i?1:0)+strnlen(name, len)+1+strnlen(value, len)+38+0+4+1) <= (FREEBOX_REQUEST_SIZE_MAX-len))
-    {
-      /* Add a separator only if it is not the first parameter */
-      if(i>0) { strncat(buff_send, "&", 2); }
-      /* Concat this name */
-      strncat(buff_send, name, len);
-      /* Concat the separator */
-      strncat(buff_send, "=", 2);
-      /* Concat this value */
-      strncat(buff_send, value, len);
-    }
-    else
-    {
-      /* If these is not enought space, continue without the others arguments */
-      break;
-    }
-  }
-
-  buff_recv = http_request("212.027.040.254", 80, buff_send, NULL, 0);
-  if(buff_send != NULL) { free(buff_send); }
-  if(buff_recv != NULL) { free(buff_recv); }
-  
   return 0;
 }
