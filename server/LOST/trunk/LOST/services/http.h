39d1996b8f93bf267ab35dbc754a9f96d538d4db
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-04-30 14:29:30 +0000

    Use stream instead of raw socket

--- server/LOST/trunk/LOST/services/http.h
+++ server/LOST/trunk/LOST/services/http.h
@@ -1,13 +1,18 @@
 #ifndef _HTTP_H_
 #define _HTTP_H_
 
+#include <stdio.h>
 #include <sys/thread.h>
+#include <sys/socket.h>
+#include <pro/httpd.h>
 
 __BEGIN_DECLS
 
 extern uint8_t http_init(void);
 extern void HttpD(void *arg) __attribute__ ((noreturn));
-extern char* http_request(char* ip, uint16_t port, char* req, char* host_req, uint16_t len_to_recv);
+extern uint8_t http_request_get_start(char* ip, uint16_t port, TCPSOCKET **sock, FILE **stream);
+extern void http_request_get_end(char* host_req, FILE *stream);
+extern void http_request_close(TCPSOCKET **sock, FILE **stream);
 extern uint8_t http_status_get(void);
 extern uint8_t http_email_send(char* msg);
 
