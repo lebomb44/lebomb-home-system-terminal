b6b5fa57d1587087c0a911ee4fe6145f1e080e8e
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-04-30 15:26:40 +0000

    Manage GET and POST methods

--- server/LOST/trunk/LOST/services/http.h
+++ server/LOST/trunk/LOST/services/http.h
@@ -10,8 +10,8 @@ __BEGIN_DECLS
 
 extern uint8_t http_init(void);
 extern void HttpD(void *arg) __attribute__ ((noreturn));
-extern uint8_t http_request_get_start(char* ip, uint16_t port, TCPSOCKET **sock, FILE **stream);
-extern void http_request_get_end(char* host_req, FILE *stream);
+extern uint8_t http_request_header_start(char* ip, uint16_t port, int method, TCPSOCKET **sock, FILE **stream);
+extern void http_request_header_end(char* host_req, FILE *stream);
 extern void http_request_close(TCPSOCKET **sock, FILE **stream);
 extern uint8_t http_status_get(void);
 extern uint8_t http_email_send(char* msg);
