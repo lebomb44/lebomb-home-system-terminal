fbe96e52816962e3a9b282782705abbc1a69a0db
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-05-01 18:22:38 +0000

    Return status on email send process

--- server/LOST/trunk/LOST/www/email_lb.php
+++ server/LOST/trunk/LOST/www/email_lb.php
@@ -1,5 +1,5 @@
 <?php
-  $from_mail = 'mms@lost.fr';
+  $from_mail = 'lb@lost.fr';
   $header  = 'MIME-Version: 1.0'."\r\n";
   $header .= 'Content-type: text/plain; charset=iso-8859-1'."\r\n";
   $header .= 'Content-transfer-encoding: 8bit'."\r\n";
@@ -10,10 +10,10 @@
   $header .= 'X-MSMail-Priority: High'."\r\n";
   $header .= 'Importance: High'."\r\n";
   if(isset($_GET['msg']) && !empty($_GET['msg'])) { $msg=$_GET['msg']; } else { $msg="UNKNOWN ALERT"; }
-  $sujet = 'LOST Alert MM : '.$msg;
+  $sujet = 'LOST Alert LB : '.$msg;
   $message = $sujet."\r\n".'Sent by '.$from_mail.' at '.date('H:i:s  d.m.Y')."\r\n-- LOST\r\n";
 
-  /* TODO set the email adresses */
+  /* Set the email adresses */
   $status_mail1 = mail("cambon.olivier@gmail.com", $sujet, $message, $header);
 
   if((status_mail1 == true) && (status_mail2 == true)) { print("LOST Email OK\r\n"); } else { print("LOST Email KO\r\n"); }
