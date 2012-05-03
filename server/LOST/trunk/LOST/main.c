6aaa0303fe17c763e49253f2968b94de919507c7
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-05-03 22:35:32 +0000

    Insert data in the DB every 15 minutes

--- server/LOST/trunk/LOST/main.c
+++ server/LOST/trunk/LOST/main.c
@@ -39,19 +39,6 @@
 
 #include "config.h"
 
-void xml_build(FILE * stream)
-{
-  fprintf_XML_header(stream);
-  fprintf_XML_elt_header("Lost", stream);
-  mon_xml_get(stream);
-  rooms_xml_get(stream);
-  events_xml_get(stream);
-  alarm_xml_get(stream);
-  safety_xml_get(stream);
-  ext_xml_get(stream);
-  fprintf_XML_elt_trailer("Lost", stream);
-}
-
 int xml_get_form(FILE * stream, REQUEST * req)
 {
   NutHttpSendHeaderTop(stream, req, 200, "Ok");
@@ -59,59 +46,91 @@ int xml_get_form(FILE * stream, REQUEST * req)
 
   if (req->req_method == METHOD_GET)
   {
-    xml_build(stream);
+    fprintf_XML_header(stream);
+    fprintf_XML_elt_header("Lost", stream);
+    mon_xml_get(stream);
+    rooms_xml_get(stream);
+    events_xml_get(stream);
+    alarm_xml_get(stream);
+    safety_xml_get(stream);
+    ext_xml_get(stream);
+    fprintf_XML_elt_trailer("Lost", stream);
   }
   fflush(stream);
 
   return 0;
 }
 
-THREAD(XMLPost, arg)
+uint8_t sql_send_once(void)
 {
   TCPSOCKET *sock = NULL;
   FILE *stream = NULL;
   uint8_t i = 0;
-char* buff = NULL;
+  char* buff = NULL;
+  char* out = NULL;
+
+  /* Connect and send the HTTP header */
+  if(http_request_header_start("212.27.63.157", 80, METHOD_POST, &sock, &stream) == 0)
+  {
+    /* Send the URL */
+    fputs(LOST_INSERT, stream);
+    /* Send the host target */
+    http_request_header_end("lebomb.free.fr", ((sizeof("roomXX.temp_value=XXX")-1)*ROOM_MAX)+ROOM_MAX-1+sizeof("&safety.ups_temp=XXXXX")-1+sizeof("&safety.rack_temp=XXXXX")-1, stream);
+    /* Build the POST request */
+    for(i=0; i<ROOM_MAX; i++)
+    {
+      if(i>0) { fputs("&", stream); }
+      fprintf(stream, "room%02d_temp_value=%03d", i, room_temp_value_get(i));
+    }
+    fprintf(stream, "&safety_ups_temp=%05d", safety_ups_temp_value_get());
+    fprintf(stream, "&safety_rack_temp=%05d", safety_rack_temp_value_get());
+    fflush(stream);
+    /* Catch the answer */
+    buff = malloc(400);
+    if(buff != NULL)
+    {
+      while(fgets(buff, 400, stream))
+      {
+        /* Force the end of the string */
+        buff[399] = '\0';
+        /* On each string search the good answer */
+        out = strstr(buff, "SQL Insert OK");
+        /* If the good answer is found, we can break the loop */
+        if(out) { break; }
+      }
+      /* The analyze is finished, so free the answer buffer */
+      free(buff);
+    }
+    http_request_close(&sock, &stream);
+  }
+  /* Build the return status */
+  if(out) { return 0; }
+
+  return 1;
+}
+
+THREAD(XMLPost, arg)
+{
+  uint8_t i = 0;
+
   arg = arg;
   NutThreadSetPriority(149);
 
   /* Wait 5 minutes */
-  //NutSleep(300000);
+  NutSleep(300000);
 
   while(1)
   {
-    if(http_request_header_start("88.190.253.248", 80, METHOD_POST, &sock, &stream) == 0)
+    /* Retry if error */
+    for(i=0; i<10; i++)
     {
-      /* Create the HTTP request beginning */
-      fputs(LOST_INSERT, stream);
-      http_request_header_end("www.lebomb.fr", ((sizeof("roomXX.temp_value=XXX")-1)*ROOM_MAX)+ROOM_MAX-1+sizeof("&safety.ups_temp=XXXXX")-1+sizeof("&safety.rack_temp=XXXXX")-1, stream);
-      /* Build the POST request */
-      for(i=0; i<ROOM_MAX; i++)
-      {
-        if(i>0) { fputs("&", stream); }
-        fprintf(stream, "room%02d_temp_value=%03d", i, room_temp_value_get(i));
-      }
-      fprintf(stream, "&safety_ups_temp=%05d", safety_ups_temp_value_get());
-      fprintf(stream, "&safety_rack_temp=%05d", safety_rack_temp_value_get());
-      fflush(stream);
-buff = malloc(400);
-/* Catch the answer */
-if(buff != NULL)
-{
-  while(fgets(buff, 400, stream))
-  {
-    /* Force the end of the string */
-    buff[399] = '\0';
-    puts(buff);
-  }
-  /* The analyze is finished, so free the answer buffer */
-  free(buff);
-}
-      http_request_close(&sock, &stream);
+      /* If OK break the retry loop */
+      if(sql_send_once() == 0) { break; }
+      /* Wait before retry */
+      NutSleep(10000);
     }
     /* Wait 15 minutes */
     NutSleep(900000);
-//NutSleep(10000);
   }
 }
 
