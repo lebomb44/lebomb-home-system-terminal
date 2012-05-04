d8b95c1e3c5eb75922e5b0202716ffcb2304c223
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-05-04 12:02:23 +0000

    Add charts and scripts for LB and BT

---
+++ server/LOST/trunk/LOST/www/charts/post_lb.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<FORM method=post action="http://lebomb.free.fr/LOST/charts/insert_lb.php">
+MnM Insert POST Form
+<TABLE>
+<?php
+for($i=0; $i<10; $i++)
+{
+  echo "<TR><TD>room".$i."_temp_value</TD><TD><INPUT type=text name='room".sprintf('%02d',$i)."_temp_value' value='".$i."'></TD></TR>";
+}
+?>
+<TR><TD>safety_ups_temp</TD><TD><INPUT type=text name="safety_ups_temp" value="123"></TD></TR>
+<TR><TD>safety_rack_temp</TD><TD><INPUT type=text name="safety_rack_temp" value="231"></TD></TR>
+<TR><TD><INPUT type="submit" value="Send"></TD></TR>
+</TABLE>
+</FORM>
+</body>
+</html>
