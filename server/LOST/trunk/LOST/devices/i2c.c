4ada03a49b9c3f59a405d8580504322ceb8cc9dc
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-05-14 17:08:18 +0000

    Recovery from error (restart I2C core component)

--- server/LOST/trunk/LOST/devices/i2c.c
+++ server/LOST/trunk/LOST/devices/i2c.c
@@ -19,7 +19,7 @@ uint8_t i2c_init(void)
 {
   uint32_t val = 0;
 
-  /* Set the pin as output with pull-up */
+  /* Set the pin as input with pull-up */
   cbi(DDRD,0);
   cbi(DDRD,1);
   sbi(PORTD,0);
@@ -55,6 +55,8 @@ uint8_t i2c_get(uint8_t sla, uint8_t addr, uint8_t nb, uint8_t* data)
   if(TwMasterTransact(sla, &addr, (uint16_t) 1, data, (uint16_t) nb, (uint32_t) 500) != ((int) nb))
   {
     ret = TwMasterError();
+    /* Restart the I2C core component */
+    TWCR = ~(1<<TWIE); NutSleep(10); TWCR = (1<<TWIE);
     /* Free the hardware interface */
     NutEventPost(&i2c_mutex);
     if(ret == 0) { return 8; } else { return ret; }
@@ -82,7 +84,7 @@ uint8_t i2c_set(uint8_t sla, uint8_t addr, uint8_t nb, uint8_t* data)
   /* Check the allocation */
   if(buff == NULL) { return 9; }
   /* Copy the data to the new area */
-  for(i=0; i<nb; i++) { buff[i+1] = data[i]; }
+  if(nb > 0) { for(i=0; i<nb; i++) { buff[i+1] = data[i]; } }
   /* But the first data is the destination address */
   buff[0] = addr;
 
@@ -100,6 +102,8 @@ uint8_t i2c_set(uint8_t sla, uint8_t addr, uint8_t nb, uint8_t* data)
     if(i==2)
     {
       ret = TwMasterError();
+      /* Restart the I2C core component */
+      TWCR = ~(1<<TWIE); NutSleep(10); TWCR = (1<<TWIE);
     }
     /* Free the hardware interface */
     NutEventPost(&i2c_mutex);
