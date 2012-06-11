974bcab1d1116a7cb45c14373d921d4be82b6c64
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-06-11 13:15:28 +0000

    Add a central index page in orger to lock the listing of the folder

---
+++ server/LOST/trunk/LOST/www/charts/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Suivi des temperatures</title>
+</head>
+<body>
+<a href="index_mms.php">M&M</a><br>
+<a href="index_lb.php">LeBomb</a><br>
+<a href="index_bt.php">Bourdilot</a><br>
+</body>
+</html>
