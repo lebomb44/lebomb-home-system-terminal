126007b5a36326ef47dfef9650e29556a53b9a62
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-09-13 19:39:36 +0000

    New version 4.10.3.1 patched at nut/arch/avr/dev/twif.c b/nut/arch/avr/dev/twif.c @@ -520,6 +520,9 @@ int NutTwiMasterTranceive(NUTTWIBUS *bus, uint8_t sla, const void *txdata, uint1
         while(icb->tw_if_busy) {
             NutSleep(63);
    +        if (tmo < 63)
    +            return -1;
    +        tmo -= 63;
         }
         NutEnterCritical();

---
+++ OS/Ethernut/2.1B_4.10.3.1/nutapp_21b/NutConf.mk
@@ -0,0 +1,18 @@
+# Automatically generated on Thu Sep 13 00:19:52 2012
+#
+# Do not edit, modify UserConf.mk instead!
+#
+
+PLATFORM=ETHERNUT2
+HWDEF+=-D$(PLATFORM)
+MCU_ATMEGA2560=atmega2560
+MCU_ATMEGA2561=atmega2561
+MCU_ATMEGA128=atmega128
+MCU_ATMEGA103=atmega103
+MCU=$(MCU_ATMEGA128)
+HWDEF+=-D__HARVARD_ARCH__
+NUTMEM_RESERVED=64
+CRUROM=crurom
+
+
+include $(top_appdir)/UserConf.mk
---
+++ OS/Ethernut/2.1B_4.10.3.1/nutapp_21b/UserConf.mk
@@ -0,0 +1,5 @@
+# Automatically created on Thu Sep 13 00:19:43 2012
+#
+# You can use this file to modify values in NutConf.mk
+#
+
