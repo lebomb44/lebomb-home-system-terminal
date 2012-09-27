c7d994bdea92d2f14376d281f9a1f3e862121a04
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-09-27 17:02:28 +0000

    We not have any node with humidity sensor. More over this pin is already used for button

--- node/ROOM/trunk/src/apps/safety.c
+++ node/ROOM/trunk/src/apps/safety.c
@@ -8,10 +8,13 @@
 #define SAFETY_TEMP_PIN  PINC
 #define SAFETY_TEMP_BIT  0
 
+/*
+These pin is already used as an input button
 #define SAFETY_HUM_DDR  DDRC
 #define SAFETY_HUM_PORT PORTC
 #define SAFETY_HUM_PIN  PINC
 #define SAFETY_HUM_BIT  1
+*/
 
 #define SAFETY_SMOKE_DDR  DDRD
 #define SAFETY_SMOKE_PORT PORTD
@@ -34,11 +37,11 @@ void safety_init(void)
 void safety_cycle(void)
 {
   node[NODE_REG_TEMP_VALUE] = adc_get(SAFETY_TEMP_BIT);
-//if(bit_is_set(SAFETY_HUM_PIN  , SAFETY_HUM_BIT  )) { node[NODE_REG_HUM_STATUS  ] = 0; } else { node[NODE_REG_HUM_STATUS  ] = 1; }
+/*if(bit_is_set(SAFETY_HUM_PIN  , SAFETY_HUM_BIT  )) { node[NODE_REG_HUM_STATUS  ] = 0; } else { node[NODE_REG_HUM_STATUS  ] = 1; }*/
   if(bit_is_set(SAFETY_SMOKE_PIN, SAFETY_SMOKE_BIT)) { node[NODE_REG_SMOKE_STATUS] = 1; } else { node[NODE_REG_SMOKE_STATUS] = 0; }
 
   if(node[NODE_REG_TEMP_MAX_CONTROL]) { if(!node[NODE_REG_TEMP_MAX_TRIG]) { if(node[NODE_REG_TEMP_VALUE  ]  > node[NODE_REG_TEMP_MAX_TH]) { node[NODE_REG_TEMP_MAX_TRIG] = 1; } } } else { node[NODE_REG_TEMP_MAX_TRIG] = 0; }
   if(node[NODE_REG_TEMP_MIN_CONTROL]) { if(!node[NODE_REG_TEMP_MIN_TRIG]) { if(node[NODE_REG_TEMP_VALUE  ]  < node[NODE_REG_TEMP_MIN_TH]) { node[NODE_REG_TEMP_MIN_TRIG] = 1; } } } else { node[NODE_REG_TEMP_MIN_TRIG] = 0; }
-//if(node[NODE_REG_HUM_CONTROL     ]) { if(!node[NODE_REG_HUM_TRIG     ]) { if(node[NODE_REG_HUM_STATUS  ]                              ) { node[NODE_REG_HUM_TRIG     ] = 1; } } } else { node[NODE_REG_HUM_TRIG     ] = 0; }
+/*if(node[NODE_REG_HUM_CONTROL     ]) { if(!node[NODE_REG_HUM_TRIG     ]) { if(node[NODE_REG_HUM_STATUS  ]                              ) { node[NODE_REG_HUM_TRIG     ] = 1; } } } else { node[NODE_REG_HUM_TRIG     ] = 0; }*/
   if(node[NODE_REG_SMOKE_CONTROL   ]) { if(!node[NODE_REG_SMOKE_TRIG   ]) { if(node[NODE_REG_SMOKE_STATUS]                              ) { node[NODE_REG_SMOKE_TRIG   ] = 1; } } } else { node[NODE_REG_SMOKE_TRIG   ] = 0; }
 }
