5200e3390d8a0e33a561fc0a71b24f8d014f27f1
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-09-28 18:42:52 +0000

    PB2 is usable on 3 different devices

--- node/ROOM/trunk/src/config.h
+++ node/ROOM/trunk/src/config.h
@@ -1,6 +1,10 @@
 #ifndef _NODE_ROOM_CONFIG_H_
 #define _NODE_ROOM_CONFIG_H_
 
+#define PB2_IR_USED       1
+#define PB2_BUTTON_USED   2
+#define PB2_LIGHT_ST_USED 3
+
 #define NODE_ROOM_CONFIG_TYPE           0x01
 #define NODE_ROOM_CONFIG_VERSION        0x01
 #define NODE_ROOM_CONFIG_HEATER_BUT     { 255, 255, 255 }
@@ -12,7 +16,7 @@
 #ifdef NODE_ROOM_CONFIG_mms_c1
 #define NODE_ROOM_CONFIG_ADDR               (10+0)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -25,7 +29,7 @@
 #ifdef NODE_ROOM_CONFIG_mms_c2
 #define NODE_ROOM_CONFIG_ADDR               (10+1)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -38,7 +42,7 @@
 #ifdef NODE_ROOM_CONFIG_mms_c3
 #define NODE_ROOM_CONFIG_ADDR               (10+2)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -51,7 +55,7 @@
 #ifdef NODE_ROOM_CONFIG_mms_c4
 #define NODE_ROOM_CONFIG_ADDR               (10+3)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -60,10 +64,11 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 1, 255, 255 }
 #endif
 
+/* Combles */
 #ifdef NODE_ROOM_CONFIG_mms_combles
 #define NODE_ROOM_CONFIG_ADDR               (10+4)
 #define QUARTZ_USED
-#define ELEC_USED
+#define PB2_USAGE                           PB2_LIGHT_ST_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -72,10 +77,11 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 1, 255, 255 }
 #endif
 
+/* Bureau */
 #ifdef NODE_ROOM_CONFIG_mms_bureau
 #define NODE_ROOM_CONFIG_ADDR               (10+5)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -84,8 +90,10 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 1, 255, 255 }
 #endif
 
+/* Salon */
 #ifdef NODE_ROOM_CONFIG_mms_salon
 #define NODE_ROOM_CONFIG_ADDR               (10+6)
+#define PB2_USAGE                           PB2_BUTTON_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 2, 4 }
@@ -94,6 +102,7 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 1, 3, 5 }
 #endif
 
+/* Cuisine */
 #ifdef NODE_ROOM_CONFIG_mms_cuisine
 #define NODE_ROOM_CONFIG_ADDR               (10+7)
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 2, 3, 4 }
@@ -104,10 +113,11 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 1, 255, 255 }
 #endif
 
+/* Couloir */
 #ifdef NODE_ROOM_CONFIG_mms_couloir
 #define NODE_ROOM_CONFIG_ADDR               (10+8)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
@@ -116,10 +126,11 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 1, 255, 255 }
 #endif
 
+/* Salon2 */
 #ifdef NODE_ROOM_CONFIG_mms_salon2
 #define NODE_ROOM_CONFIG_ADDR               (10+9)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 255, 255, 255 }
@@ -128,10 +139,11 @@
 #define NODE_ROOM_CONFIG_SHUTTER_RELAY_DOWN { 255, 255, 255 }
 #endif
 
+/* Terrasse */
 #ifdef NODE_ROOM_CONFIG_mms_terrasse
 #define NODE_ROOM_CONFIG_ADDR               (10+10)
 #define QUARTZ_USED
-#define IR_USED
+#define PB2_USAGE                           PB2_IR_USED
 #define NODE_ROOM_CONFIG_LIGHT_BUT          { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_LIGHT_RELAY        { 255, 255, 255 }
 #define NODE_ROOM_CONFIG_SHUTTER_BUT_UP     { 0, 255, 255 }
