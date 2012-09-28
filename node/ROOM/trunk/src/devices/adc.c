becaf4ac26797cfdf18c4ae7e1412e7d510aa059
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-09-28 00:15:17 +0000

    Turn off ADC when not used

--- node/ROOM/trunk/src/devices/adc.c
+++ node/ROOM/trunk/src/devices/adc.c
@@ -7,14 +7,18 @@ void adc_init(void)
 
 u08 adc_get(u08 channel)
 {
-  uint16_t temp_ADC = 0;
+  u16 temp_ADC = 0;
+  u08 ret = 0;
 
-  if(channel>7) { return 0; }
+  if(channel>5) { return 0; }
   ADMUX=(0x40+channel);
   ADCSRA=0xD7; /* 0b11010111 */
   while(!bit_is_set(ADCSRA,4)); /* 0b00010000 */
-  temp_ADC = (uint16_t) ADCL;
-  temp_ADC += (((uint16_t) ADCH) << 8);
+  temp_ADC = (u16) ADCL;
+  temp_ADC += (((u16) ADCH) << 8);
+  ADCSRA = 0;
 
-  return temp_ADC;
+  if(temp_ADC > 255) { ret = 255; }
+  else { ret = temp_ADC; }
+  return ret;
 }
