dc338a1d3ab0825d8c979b1a104d736f82c4c880
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2012-09-28 16:54:41 +0000

    You must use START to check the end of conversion

--- node/ROOM/trunk/src/devices/adc.c
+++ node/ROOM/trunk/src/devices/adc.c
@@ -13,7 +13,7 @@ u08 adc_get(u08 channel)
   if(channel>5) { return 0; }
   ADMUX=(0x40+channel);
   ADCSRA=0xD7; /* 0b11010111 */
-  while(!bit_is_set(ADCSRA,4)); /* 0b00010000 */
+  while(bit_is_set(ADCSRA, ADSC));
   temp_ADC = (u16) ADCL;
   temp_ADC += (((u16) ADCH) << 8);
   ADCSRA = 0;
