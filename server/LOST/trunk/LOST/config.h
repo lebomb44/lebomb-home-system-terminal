2eb4fade0c030dba0359c6e64a6a5131d816bd5a
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2013-03-01 18:46:30 +0000

    email php scripts moved to safety folder

--- server/LOST/trunk/LOST/config.h
+++ server/LOST/trunk/LOST/config.h
@@ -5,21 +5,21 @@
 #define LOST_GSM_SMS_CENTER_FREE
 #define LOST_GSM1 "0683585066"
 #define LOST_GSM2 "0689350159"
-#define LOST_EMAIL "email_mms.php?msg="
+#define LOST_EMAIL "safety/email_mms.php?msg="
 #define LOST_INSERT "LOST/charts/insert_mms.php"
 #endif
 #ifdef LOST_CONFIG_bt
 #define LOST_GSM_SMS_CENTER_FREE
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
-#define LOST_EMAIL "email_bt.php?msg="
+#define LOST_EMAIL "safety/email_bt.php?msg="
 #define LOST_INSERT "LOST/charts/insert_bt.php"
 #endif
 #ifdef LOST_CONFIG_lb
 #define LOST_GSM_SMS_CENTER_SFR
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
-#define LOST_EMAIL "email_lb.php?msg="
+#define LOST_EMAIL "safety/email_lb.php?msg="
 #define LOST_INSERT "LOST/charts/insert_lb.php"
 #endif
 
