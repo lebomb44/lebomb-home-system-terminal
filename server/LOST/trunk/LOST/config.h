263388de92145d17852741613527ecc4ce2fd221
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2013-03-06 22:53:33 +0000

    Rename files

--- server/LOST/trunk/LOST/config.h
+++ server/LOST/trunk/LOST/config.h
@@ -5,24 +5,24 @@
 #define LOST_GSM_SMS_CENTER_FREE
 #define LOST_GSM1 "0683585066"
 #define LOST_GSM2 "0689350159"
-#define LOST_STATUS "safety/mms_status.php"
-#define LOST_EMAIL "safety/mms_email.php?msg="
+#define LOST_STATUS "LOST/safety/mms_status.php"
+#define LOST_EMAIL "LOST/safety/mms_email.php?msg="
 #define LOST_INSERT "LOST/charts/mms_insert.php"
 #endif
 #ifdef LOST_CONFIG_bt
 #define LOST_GSM_SMS_CENTER_FREE
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
-#define LOST_STATUS "safety/bt_status.php"
-#define LOST_EMAIL "safety/bt_email.php?msg="
+#define LOST_STATUS "LOST/safety/bt_status.php"
+#define LOST_EMAIL "LOST/safety/bt_email.php?msg="
 #define LOST_INSERT "LOST/charts/bt_insert.php"
 #endif
 #ifdef LOST_CONFIG_lb
 #define LOST_GSM_SMS_CENTER_SFR
 #define LOST_GSM1 "0689350159"
 #define LOST_GSM2 "0689350159"
-#define LOST_STATUS "safety/lb_status.php"
-#define LOST_EMAIL "safety/lb_email.php?msg="
+#define LOST_STATUS "LOST/safety/lb_status.php"
+#define LOST_EMAIL "LOST/safety/lb_email.php?msg="
 #define LOST_INSERT "LOST/charts/lb_insert.php"
 #endif
 
