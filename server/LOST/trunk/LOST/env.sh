b84ab604a6def00c56c4a9973827f10566976a5b
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2013-07-02 20:30:10 +0000

    It seems that is does not works with Nut/OS 4.10.3.1

--- server/LOST/trunk/LOST/env.sh
+++ server/LOST/trunk/LOST/env.sh
@@ -1,7 +1,7 @@
 export DEVBASE_DRIVE=/media/D
 export DEVBASE=$DEVBASE_DRIVE/Eclipse_WS
 export COMPILER_VERSION=WinAVR_20100110
-export OS_VERSION=2.1B_4.10.3.1
+export OS_VERSION=2.1B_4.10.1.0
 # Burn serial port for avrdude
 export BURNPORT=/dev/ttyS0
 
