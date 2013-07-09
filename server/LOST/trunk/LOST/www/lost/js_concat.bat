99b549152fc6c08bd5d7c68c00162db64281a6c7
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2013-07-09 12:47:34 +0000

    Add pages for new target FR

--- server/LOST/trunk/LOST/www/lost/js_concat.bat
+++ server/LOST/trunk/LOST/www/lost/js_concat.bat
@@ -13,8 +13,6 @@ type com\lost_safety_html.js >> mms.js
 type mms\lost_rooms_html.js >> mms.js
 type mms\lost_ev_html.js >> mms.js
 
-type mms.js > lb.js
-
 type com\lost.js > bt.js
 type com\lost_mon.js >> bt.js
 type com\lost_alarm.js >> bt.js
@@ -29,3 +27,33 @@ type bt\lost_atm_html.js >> bt.js
 type com\lost_safety_html.js >> bt.js
 type bt\lost_rooms_html.js >> bt.js
 type bt\lost_ev_html.js >> bt.js
+
+type com\lost.js > lb.js
+type com\lost_mon.js >> lb.js
+type com\lost_alarm.js >> lb.js
+type com\lost_atm.js >> lb.js
+type com\lost_safety.js >> lb.js
+type com\lost_rooms.js >> lb.js
+type com\lost_ev.js >> lb.js
+type lb\lost_html.js >> lb.js
+type com\lost_mon_html.js >> lb.js
+type com\lost_alarm_html.js >> lb.js
+type lb\lost_scenes_html.js >> lb.js
+type com\lost_safety_html.js >> lb.js
+type lb\lost_rooms_html.js >> lb.js
+type lb\lost_ev_html.js >> lb.js
+
+type com\lost.js > fr.js
+type com\lost_mon.js >> fr.js
+type com\lost_alarm.js >> fr.js
+type com\lost_atm.js >> fr.js
+type com\lost_safety.js >> fr.js
+type com\lost_rooms.js >> fr.js
+type com\lost_ev.js >> fr.js
+type fr\lost_html.js >> fr.js
+type com\lost_mon_html.js >> fr.js
+type com\lost_alarm_html.js >> fr.js
+type fr\lost_atm_html.js >> fr.js
+type com\lost_safety_html.js >> fr.js
+type fr\lost_rooms_html.js >> fr.js
+type fr\lost_ev_html.js >> fr.js
