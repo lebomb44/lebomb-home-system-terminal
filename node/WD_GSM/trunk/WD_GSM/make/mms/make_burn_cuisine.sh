f11816eb1925a8a6bfce76628bd8d53457ee8bcf
cambon.olivier@gmail.com <cambon.olivier@gmail.com@255e6015-6515-fece-5b3f-62a74f1426f9>
2014-01-16 13:12:47 +0000

    Watchdog to monitor UART activity

--- node/WD_GSM/trunk/WD_GSM/make/mms/make_burn_cuisine.sh
+++
@@ -1,8 +0,0 @@
-source ../../env.sh
-cd $DEVBASE_DRIVE
-cd $DEVBASE/NODE_ROOM
-echo $PATH
-make clean
-export NODE_REMOTE_CONFIG=mms_cuisine
-make burn
-
--- node/WD_GSM/trunk/WD_GSM/make/mms/make_burn_global.sh
+++
@@ -1,8 +0,0 @@
-source ../../env.sh
-cd $DEVBASE_DRIVE
-cd $DEVBASE/NODE_REMOTE
-echo $PATH
-make clean
-export NODE_REMOTE_CONFIG=global
-make burn
-
